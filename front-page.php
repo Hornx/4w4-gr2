d85a7dab233fad4674249d7d42859477e8e78c32
Marc-Anderson  Gilles <hornil6748@gmail.com>
2022-03-18 11:46:26 -0400

    -m 's8 c2 ajout sidebar pour le front-page

--- front-page.php
+++ front-page.php
@@ -9,7 +9,12 @@
     <div class="animation__bloc">5</div>
 </section>
 
+
     <h1>---- Front-page.php ------</h1>
+    <section class="contenuText">
+    <?= get_sidebar('front_page_1'); ?>
+    <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+    </section>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>   
