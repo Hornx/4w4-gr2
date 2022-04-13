a0c8539e155d70d409f6291883b3e03041ccb193
Marc-Anderson  Gilles <hornil6748@gmail.com>
2022-04-12 23:49:50 -0400

    -m commit s11 MA ajout pour l'exer 3

--- functions.php
+++ functions.php
@@ -144,12 +144,72 @@ function my_register_sidebars() {
 }
 
 /* ----------------------------------------------------------------- */
+ /**
+  * Extraire le slug de la catégorie de l'url 
+  * @param array $tableau : liste des slug de catégorie de la page
+  * @return string $cle : le slug de la catégorie  
+  */
 function trouve_la_categorie($tableau){
     foreach($tableau as $cle){
         if(is_category($cle)) return($cle);
     }
 }
 
+ /**
+ * Modifie la requête global de WP_query.
+ * 
+ * @param WP_query $query : Objet contenant la requête global.
+ * @return WP_query $query. 
+ */
+function cidw_4w4_pre_get_posts(WP_Query $query)
+{
+    if (is_admin() 
+        || !is_main_query() 
+        || !is_category(array('cours','web','jeu','design','utilitaire','creation-3d','video'))   )
+    {
+        return $query;
+    }        
+    else
+    {
+        $ordre = get_query_var('ordre');
+        $cle = get_query_var('cletri');       
+        $query->set('order',  $ordre);
+        $query->set('orderby', $cle);
+        /*
+        $query->set('order',  'asc');
+        $query->set('orderby', 'title');
+        */
+        $query->set('postperpage','-1');
+        return $query;
+    }
+}
+
+
+
+function cidw_4w4_query_vars($params){
+// var_dump($params);
+// die();
+    $params[] = "ordre";
+    $params[] = "cletri";
+return $params;
+
+}
+/*
+    $params[] = "cletri";
+    $params[] = "ordre";
+    //$params["cletri"] = "title";
+    //var_dump($params); die();
+    return $params;
+}
+
+*/
+add_action('pre_get_posts', 'cidw_4w4_pre_get_posts');
+/* 
+Que le hook «pre_get_posts» se manisfeste juste avant que la requête WP_query soit exécuté. 
+Ce hook nous permettra d'adapter la requête avant d'exécuter cette requête 
+*/ 
+
+add_filter('query_vars', 'cidw_4w4_query_vars' );
 
 
 
--- gabarit/content-macrame.php
+++ gabarit/content-macrame.php
@@ -1,4 +1,22 @@
-<article class='cours_macrame'>
-    <?php the_title(); ?>
+<?php 
+
+                // get_category_by_slug()
+    $categorie = get_the_category();
+    // var_dump($categorie);
+    // echo $categorie[1]->slug
+
+?>
+<article class="formation__cours  <?php  echo $categorie[1]->slug; ?>">
+    <?php          
+    $titre = get_the_title();
+  
+    ?>
+    <?php the_post_thumbnail("thumbnail"); ?>
+    <h3 class="cours__titre">
+        <a href="<?php echo get_permalink(); ?>">
+            <?= $titre; ?>
+        </a>
+    </h3>
+    <?php the_category(); ?>
     <?php the_content(); ?>
 </article>
