eaab3c87a0093ea94222901da7a7e708c29418af
Marc-Anderson  Gilles <hornil6748@gmail.com>
2022-04-23 16:53:25 -0400

    -commit m MA s12 c1 lab6
    update de du lab6

--- gabarit/content-cours.php
+++ gabarit/content-cours.php
@@ -1,12 +1,12 @@
 <?php 
 
                 // get_category_by_slug()
-    $categorie = get_the_category();
+    $categories = get_the_category();
     // var_dump($categorie);
     // echo $categorie[1]->slug
 
 ?>
-<article class="formation__cours  <?php  echo $categorie[1]->slug; ?>">
+<article class="formation__cours  <?php  echo $categories[1]->slug; ?>">
     <?php          
     $titre = get_the_title();
     $titreFiltreCours = substr($titre, 3, -6);
