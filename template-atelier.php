b6d5f0aa9da6fe64d5a404a81ca3031ae002a4a2
Marc-Anderson  Gilles <hornil6748@gmail.com>
2022-05-19 15:05:52 -0400

    MA s15 epreuve-Finale améloration du template Atelier

--- template-atelier.php
+++ template-atelier.php
@@ -16,7 +16,7 @@
    <h1><?php the_title() ?></h1>
    <?php the_content() ?> 
    
-
+     <h2>Description de l'atelier</h2>
    <p class="atelier__description_de_l’atelier">
         <?php the_field('description_de_l’atelier'); ?>
    </p>
@@ -24,12 +24,12 @@
    <p class="atelier__animateur">
         <?php the_field('animateur'); ?>
    </p>
-
+     <h2>Horaire et dates de l'atelier </h2>
    <p class="atelier__date_de_debut_de_l’atelier">
-        <?php the_field('date_de_debut_de_l’atelier'); ?>
+     Date de début :   <?php the_field('date_de_debut_de_l’atelier'); ?>
    </p>
    <p class="atelier__date_de_fin_de_l’atelier">
-        <?php the_field('date_de_fin_de_l’atelier'); ?>
+     Date de fin :   <?php the_field('date_de_fin_de_l’atelier'); ?>
    </p>
 
    <p class="atelier__jours_de_la_semaine_de_l’atelier">
@@ -37,14 +37,14 @@
    </p>
 
    <p class="atelier__heure_de_debut">
-        <?php the_field('heure_de_debut'); ?>
+     L'horaire : De    <?php the_field('heure_de_debut'); ?> à <?php the_field('heure_de_fin'); ?>
    </p>
-   <p class="atelier__heure_de_fin">
-        <?php the_field('heure_de_fin'); ?>
+ <p class="atelier__heure_de_fin"> 
+         
    </p>
 
    <p class="atelier__duree_d’une_seance_d’atelier">
-        <?php the_field('duree_d’une_seance_d’atelier'); ?>
+     Durée de chacune des séances est de  <?php the_field('duree_d’une_seance_d’atelier'); ?> heures
    </p>
 
    <p class="atelier__local_ou_se_deroulera_l’atelier">
