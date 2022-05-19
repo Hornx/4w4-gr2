6fff695fc74e636fc8740eee28c617643f1f9f6a
Marc-Anderson  Gilles <hornil6748@gmail.com>
2022-05-19 13:23:28 -0400

    MA s15 epreuve finale arrangement

--- template-atelier.php
+++ template-atelier.php
@@ -6,15 +6,3 @@
 * @subpackage cidw_4w4
 */
 ?>
-
-
-<?php get_header() ?>
-<main class="site__main">
-
-    <h1>---- Template attelier ------</h1>
-   <?php if (have_posts()): the_post(); ?>
-        <?php the_title() ?>
-        <?php the_content() ?>        
-   <?php endif ?>
-</main>
-<?php get_footer() ?>
